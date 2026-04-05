6e0a26b3fc5b7447b04810ecb860e4c256448761
Abhishek Chandravanshi <abhishekchandrawanshi20@gmail.com>
2026-04-06 01:15:05 +0530

    Initial commit

---
+++ PYTHON/lec_01.py
@@ -0,0 +1,20 @@
+from builtins import input, int, print,float
+
+# x=5
+# y=7
+# z=5
+# print(id(x),id(y),id(z))        #  id() is a built in function which gives the address of the variable in memory
+# y=9
+# print(id(x),id(y),id(z))
+
+ 
+
+
+
+
+# Write a program to print  lenght and width of a rectangle and calculate its area 
+
+length= float(input("Enter the length of the rectangle: "))
+width= float(input("Enter the width of the rectangle: "))   
+area= length*width
+print("The area of the rectangle is: ",area)
