b3366d827bd7d38fc8d3cd1bf41f53e6518b0973
ABHISHEK CHANDRAVANSHI <abhishekchandrawanshi20@gmail.com>
2026-04-07 12:53:14 +0530

    Initial commit

--- PYTHON/lec-03.py
+++ PYTHON/lec-03.py
@@ -33,17 +33,37 @@ from builtins import input, int, print, range
         
 #      -------x-------x---------x-----------x--- 
        
+# # 
 # 4. Write a program to count the even and odd factors of a number
-n = int(input("Enter a number:  "))
-even_count = 0
-odd_count = 0
-for i in range(1,n+1):
-    if n%i == 0:
-        if i%2 == 0:
-            even_count += 1
-        else:
-            odd_count += 1
-print("Even factors:", even_count)
-print("Odd factors:", odd_count)
+# n = int(input("Enter a number:  "))
+# even_count = 0
+# odd_count = 0
+# for i in range(1,n+1):
+#     if n%i == 0:
+#         if i%2 == 0:
+#             even_count += 1
+#         else:
+#             odd_count += 1
+# print("Even factors:", even_count)
+# print("Odd factors:", odd_count)
 
 #     -------x-------x---------x-----------x---
+
+
+
+# n = int(input("Enter a number:  "))
+# for i in range(2, n//2):
+#     if n/i == 0:
+#         print("The number is not prime")
+#         break
+# else:
+#     print("The number is prime")
+
+# write a program which will take input from user and check the count of digits in the number and print them
+
+n = int(input("Enter a number:  "))
+count = 0 
+while n > 0:
+    n = n//10
+    count += 1
+print("The count of digits in the number is: ", count)
