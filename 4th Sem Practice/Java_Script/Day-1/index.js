7b88c2603b2147d4b3139048885f15d32f5ccf3d
ABHISHEK CHANDRAVANSHI <abhishekchandrawanshi20@gmail.com>
2026-05-06 11:35:20 +0530

    commit

---
+++ 4th Sem Practice/Java_Script/Day-1/index.js
@@ -0,0 +1,29 @@
+
+            // console.log("Hii")
+            // console.error("error")
+            // // console.warn("warn")
+            // console.log(5==5)   //true
+            // console.log(5==="5")//false
+            // console.log(5+'5')  //55
+            // console.log(5-'5')  //0
+            // console.log(false+5)//5
+            // console.log(null+null)//0
+            // console.log(undefined-1) //NaN
+            // console.log(null+"hii") //nullhii
+            // console.log(1+2+"2");
+            
+            // console.log(typeof (typeof(5)))
+
+            // console.log( {}=={} )  //false
+
+
+            if(true){
+                console.log("hello")
+            }else{
+                console.log("hii")
+            }
+
+
+
+             
+
