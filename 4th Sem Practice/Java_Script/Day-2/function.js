7ae781c2b653a38dc4dc1f3f601a91821c73625c
ABHISHEK CHANDRAVANSHI <abhishekchandrawanshi20@gmail.com>
2026-05-08 11:34:26 +0530

    commit

---
+++ 4th Sem Practice/Java_Script/Day-2/function.js
@@ -0,0 +1,28 @@
+
+// // function 
+//     let a=5;
+//     function sum() {
+//         let b=10;
+//         console.log(a);
+//         console.log(b);
+//     }
+//     sum()  //5, 10
+
+
+
+    // let a=5;
+    // function outer() {
+    //     console.log(a);
+
+    //     function inner(){
+    //         console.log("hello")
+    //     }
+    //     inner()
+        
+    // }
+    // outer()
+
+
+    // Hoisting
+    console.log(a);
+    let a=5 
