c9f72027b4599d1340f122a8f41fa16e7e279944
ABHISHEK CHANDRAVANSHI <abhishekchandrawanshi20@gmail.com>
2026-05-11 13:14:03 +0530

    commit

--- 4th Sem Practice/Java_Script/Day-4/DOM.js
+++ 4th Sem Practice/Java_Script/Day-4/DOM.js
@@ -1,22 +1,22 @@
 // DOM :- Document Object Model
 
-//  let btn= document.querySelector(".two")
+ let btn= document.querySelector(".two")
 
-// //  let btn= document.getElementById("one")    // this line also working
+//  let btn= document.getElementById("one")    // this line also working
 
-// //  let btn= document.getElementsByClassName("two") // class tag might be common so it can't ne used directly.
-//  let body= document.querySelector("body")
+//  let btn= document.getElementsByClassName("two") // class tag might be common so it can't ne used directly.
+ let body= document.querySelector("body")
 
-//  btn.addEventListener("click",function() {
-//     body.style.backgroundColor="pink"
-//  })
+ btn.addEventListener("click",function() {
+    body.style.backgroundColor="pink"
+ })
 
 
-let h1= document.getElementsByClassName("two")
+// let h1= document.getElementsByClassName("two")
 
-let btn= document.querySelector("button")
-btn.addEventListener("click",function(){
+// let btn= document.querySelector("button")
+// btn.addEventListener("click",function(){
    
-})
+// })
 
 
