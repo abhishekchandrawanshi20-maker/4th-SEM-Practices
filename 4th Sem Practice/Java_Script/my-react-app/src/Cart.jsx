3f6e6f078717a52eb4a1393dc2dea06d002e831c
ABHISHEK CHANDRAVANSHI <abhishekchandrawanshi20@gmail.com>
2026-06-10 15:10:19 +0530

    useContext

--- 4th Sem Practice/Java_Script/my-react-app/src/Cart.jsx
+++ 4th Sem Practice/Java_Script/my-react-app/src/Cart.jsx
@@ -1,15 +1,43 @@
-// import React from 'react'
-import './App.css'
+// // import React from 'react'
+// import './App.css'
+// const Cart = ({cart}) => {
+//   return (
+//     <div>
+//           {
+//         cart.map((a)=>{
+//           return(<div id='main_card'>
+//           <img   src={a.thumbnail}/>
+//           {/* <h4>{a.title}</h4>
+//           <h6>{a.price}</h6> */}
+
+//           </div>)
+//         })
+//       }
+//     </div>
+//   )
+// }
+
+// export default Cart
+
+
+
+
+
+
+
+
+import React from 'react'
+
 const Cart = ({cart}) => {
   return (
     <div>
-          {
+         {
         cart.map((a)=>{
           return(<div id='main_card'>
-          <img   src={a.thumbnail}/>
-          {/* <h4>{a.title}</h4>
-          <h6>{a.price}</h6> */}
-
+          <img  src={a.thumbnail}/>
+          <p>{a.title}</p>
+          <p>{a.price}</p>
+           
           </div>)
         })
       }
