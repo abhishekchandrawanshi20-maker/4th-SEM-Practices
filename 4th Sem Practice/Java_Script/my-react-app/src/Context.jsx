7775f2a60e0cecd00b1c14b6620306958437f76d
ABHISHEK CHANDRAVANSHI <abhishekchandrawanshi20@gmail.com>
2026-06-10 15:14:43 +0530

    useContext

--- 4th Sem Practice/Java_Script/my-react-app/src/Context.jsx
+++ 4th Sem Practice/Java_Script/my-react-app/src/Context.jsx
@@ -1,4 +1,32 @@
-import { createContext } from "react";
+import React, { createContext, useReducer } from "react";
+export const storeContext = createContext();
+const initialState = {
+      input:"",
+      todos:[]
+};
+function reducer(store, action) {
+    console.log(action,"acc");
+    
+    if(action.type=="Set_inp"){
+        return{
+            ...store,
+            input:action.payload
+        }
+    }else if(action.type=="Add_todo"){
+        return{
+            ...store,
+            todos:[...store.todos,store.input]
+        }
+    }
+}
+const Context = ({ children }) => {
+  const [store, dispatch] = useReducer(reducer, initialState);
 
- let Context=   createContext()
- export default Context
+  return (
+    <storeContext.Provider value={{ store, dispatch }}>
+      {children}
+    </storeContext.Provider>
+  );
+};
+
+export default Context;
