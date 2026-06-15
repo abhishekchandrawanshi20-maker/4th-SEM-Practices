0f1f00ebaddfd984b7032108533a7fae89fc3ff9
ABHISHEK CHANDRAVANSHI <abhishekchandrawanshi20@gmail.com>
2026-06-15 11:07:17 +0530

    commit

---
+++ 4th Sem Practice/DSA/lec-15/price.java
@@ -0,0 +1,32 @@
+// //package DSA.lec-15;
+
+// public class price {
+//     public static void main(String[] args) {
+        
+// //     }
+// //     st.push(arr[i]);
+// // }
+// // System.out..println(Arrays.toString(ans));
+
+// // [100,80,60,75,85]
+// // [1,1,1,2,2,4,6,]
+
+
+
+// int princ[] = {100,80,60,70,60,75,85};
+// int n = princ.length[n];
+// int span[] = new int[n];
+// stock<Integer> st = new Stack<>();
+// for(int i=0;i<n;i++){
+//     while(!st.isEmpty() && princ[st.peek()]<=princ[i]){
+//         st.pop();
+//     }
+//     if(st.isEmpty()){
+//         span[i] = i+1;
+//     } else {
+//         span[i] = i - st.peek();
+//     }
+//     st.push(i);
+// }
+
+  
