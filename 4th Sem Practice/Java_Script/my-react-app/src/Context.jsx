603756817314f62ac7a4de9c9cc66051de348ee5
ABHISHEK CHANDRAVANSHI <abhishekchandrawanshi20@gmail.com>
2026-06-15 11:05:06 +0530

    dynamic routingg

--- 4th Sem Practice/Java_Script/my-react-app/src/Context.jsx
+++ 4th Sem Practice/Java_Script/my-react-app/src/Context.jsx
@@ -1,32 +1,32 @@
-import React, { createContext, useReducer } from "react";
-export const storeContext = createContext();
-const initialState = {
-      input:"",
-      todos:[]
-};
-function reducer(store, action) {
-    console.log(action,"acc");
+// import React, { createContext, useReducer } from "react";
+// export const storeContext = createContext();
+// const initialState = {
+//       input:"",
+//       todos:[]
+// };
+// function reducer(store, action) {
+//     console.log(action,"acc");
     
-    if(action.type=="Set_inp"){
-        return{
-            ...store,
-            input:action.payload
-        }
-    }else if(action.type=="Add_todo"){
-        return{
-            ...store,
-            todos:[...store.todos,store.input]
-        }
-    }
-}
-const Context = ({ children }) => {
-  const [store, dispatch] = useReducer(reducer, initialState);
+//     if(action.type=="Set_inp"){
+//         return{
+//             ...store,
+//             input:action.payload
+//         }
+//     }else if(action.type=="Add_todo"){
+//         return{
+//             ...store,
+//             todos:[...store.todos,store.input]
+//         }
+//     }
+// }
+// const Context = ({ children }) => {
+//   const [store, dispatch] = useReducer(reducer, initialState);
 
-  return (
-    <storeContext.Provider value={{ store, dispatch }}>
-      {children}
-    </storeContext.Provider>
-  );
-};
+//   return (
+//     <storeContext.Provider value={{ store, dispatch }}>
+//       {children}
+//     </storeContext.Provider>
+//   );
+// };
 
-export default Context;
+// export default Context;
