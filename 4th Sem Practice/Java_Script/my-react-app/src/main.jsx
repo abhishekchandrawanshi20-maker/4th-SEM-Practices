54888e3a8c18436fe61b01b19ee5de830eb78c06
ABHISHEK CHANDRAVANSHI <abhishekchandrawanshi20@gmail.com>
2026-06-15 11:05:49 +0530

    dynamic routingg

--- 4th Sem Practice/Java_Script/my-react-app/src/main.jsx
+++ 4th Sem Practice/Java_Script/my-react-app/src/main.jsx
@@ -2,15 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
+import {BrowserRouter}   from 'react-router-dom'
 
-import { BrowserRouter } from 'react-router-dom'
-import Context from './Context.jsx'
+// import Context from './Context.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Context>   
+ 
 
+  <BrowserRouter> 
     <App />
-    </Context>
+    </BrowserRouter>
+
+  
+  
+ 
  
 
 
